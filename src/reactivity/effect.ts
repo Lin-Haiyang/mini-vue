cbbd0192bc223d3545e59e9c6b9515bee2acc886
Jade <linhyang@yonyou.com>
2024-01-17 21:29:07 +0800

    fix: 优化 stop 功能的处理边缘 case

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,19 +1,29 @@
 import { extend } from "../shared";
 
+let activeEffect;
+let shouldTrack;
 class reactiveEffect {
-  private _fn
-  public scheduler: Function | undefined
+  private _fn;
+  public scheduler: Function | undefined;
   public deps: any = [];
   private active: boolean = true;
   public onStop?: () => void;
   constructor(fn, scheduler) {
     this._fn = fn;
-    this.scheduler = scheduler
+    this.scheduler = scheduler;
   }
 
   run() {
+    if (!this.active) {
+      return this._fn();
+    }
+    // 应该收起依赖
+    shouldTrack = true;
     activeEffect = this;
-    return this._fn();
+    const result = this._fn();
+    // 重置状态
+    shouldTrack = false;
+    return result;
   }
 
   stop() {
@@ -28,14 +38,16 @@ class reactiveEffect {
 }
 
 function cleanupEffect(effect) {
-  effect.deps.forEach(dep => {
+  effect.deps.forEach((dep) => {
     dep.delete(effect);
   });
+  // 把 effect.deps 清空
+  effect.deps.length = 0;
 }
-
 
 const targetMap = new Map();
 export function track(target, key) {
+  if (!isTracking()) return;
   // target -> key -> deps
   let depsMap = targetMap.get(target);
   if (!depsMap) {
@@ -48,9 +60,15 @@ export function track(target, key) {
     deps = new Set();
     depsMap.set(key, deps);
   }
-  if (!activeEffect) return;
+
+  // 看看 dep 之前有没有添加过，添加过的话 那么就不添加了
+  if (deps.has(activeEffect)) return;
   deps.add(activeEffect);
   activeEffect.deps.push(deps);
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
@@ -65,7 +83,6 @@ export function trigger(target, key) {
   }
 }
 
-let activeEffect;
 export function effect(fn, options: any = {}) {
   const _effect = new reactiveEffect(fn, options.scheduler);
   _effect.run();
