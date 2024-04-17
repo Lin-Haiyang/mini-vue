c64d57e02ccb4ec2671b6ae63994fb3677bcd348
Jade <linhyang@yonyou.com>
2024-04-17 23:39:05 +0800

    feat: 实现初始化 element 主流程

--- example/helloword/App.js
+++ example/helloword/App.js
@@ -1,8 +1,16 @@
-import { h } from "../../lib/guide-mini0-vue.esm.js";
+import { h } from "../../lib/guide-mini-vue.esm.js";
 
 export const App = {
   render() {
-    return h('div',{ id: 'box' },'hi,' + this.msg);
+    return h(
+      'div',
+      { id: 'box' },
+      [
+        h('p', { class: 'red' }, '红色'),
+        h('p', { class: 'blue' }, '蓝色'),
+        h('p', { class: 'green' }, '绿色')
+      ]
+    );
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -6,7 +7,39 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
   // 是不是 component 或者 element 类型
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+  const { props, children } = vnode;
+
+  if (typeof children === 'string') {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode, container) {
