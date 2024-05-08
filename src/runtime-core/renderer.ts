6409f483f268e92f97661c799568cec5b3bb9a75
Jade <linhyang@yonyou.com>
2024-05-08 11:11:03 +0800

    feat: 更新 element 的 props

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -3,6 +3,7 @@ import { createComponentInstance, setupComponent } from "./component";
 import { Fragment, Text } from "./vnode";
 import { createAppAPI } from "./createApp";
 import { effect } from "../reactivity/effect";
+import { EMPTY_OBJ } from "../shared";
 
 export function createRenderer(options) {
   const {
@@ -61,6 +62,31 @@ export function createRenderer(options) {
   function patchElement(n1, n2, container) {
     console.log("🚀 ~ patchElement ~ n2:", n2)
     console.log("🚀 ~ patchElement ~ n1:", n1)
+    const oldProps = n1.props || EMPTY_OBJ;
+    const newProps = n2.props || EMPTY_OBJ;
+    const el = (n2.el = n1.el);
+    patchProps(el, oldProps, newProps);
+  }
+
+  function patchProps(el, oldProps, newProps) {
+    if (oldProps !== newProps) {
+      for (const key in newProps) {
+        const prevProp = oldProps[key];
+        const nextProp = newProps[key];
+  
+        if (prevProp !== nextProp) {
+          hostPatchProp(el, key, prevProp, nextProp)
+        }
+      }
+  
+      if (oldProps !== EMPTY_OBJ) {
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
   }
 
   function mountElement(vnode, container, parentComponent) {
@@ -75,7 +101,7 @@ export function createRenderer(options) {
 
     for (const key in props) {
       const val = props[key];
-      hostPatchProp(el, key, val)
+      hostPatchProp(el, key, null, val)
     }
 
     hostInsert(el, container)
